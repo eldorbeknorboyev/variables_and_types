3fb6da2803d96cd8a6dd434d6624566d40e5e7f7
eldorbeknorboyev <144788922+eldorbeknorboyev@users.noreply.github.com>
2023-09-20 12:25:25 +0500

    done

---
+++ begin01.py
@@ -0,0 +1,6 @@
+# Assign 5 (int value) to 'num_one' variable;
+num_one=5
+# Assign 4 (int value) to 'num_two' variable;
+num_two=4
+# Print sum of 'num_one' and 'num_two' variable;
+print(num_one+num_two)
